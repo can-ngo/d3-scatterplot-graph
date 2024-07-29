45bb0075d042c498632f1e4f762b34b59af1225f
Can Ngo <44318634+nncan99@users.noreply.github.com>
2024-07-29 19:17:33 +0700

    Add and modified x-axis

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ fetch(url)
     .then(res => res.json())
     .then(data => {
 
-        const years = data.map(item => item.Year)
-                              .sort((a,b)=>a-b);
+        //Convert the year format to Date objects
+        const yearToDate = year => new Date(year,0,1);
+
+        const yearsDate = data.map(item => yearToDate(item.Year)).sort((a,b)=>a-b);
         
         //Convert the time from string 'mm:ss' to Date object
         const timeToDate = timeString => {
@@ -19,25 +21,40 @@ fetch(url)
         const times = data.map(item => timeToDate(item.Time));
              
         const width = 800;
-        const height = 400;
+        const height = 500;
 
-        const yearMax = d3.max(years);
-        const yearMin = d3.min(years);
+        const yearMax = d3.max(yearsDate);
+              yearMax.setMonth(yearMax.getMonth()+12)//For display purpose
+        const yearMin = d3.min(yearsDate);
+              yearMin.setMonth(yearMin.getMonth()-12)//For display purpose
         const timeMax = d3.max(times);
         const timeMin = d3.min(times);
 
         //Create SVG container
         const svg = d3.select('.container')
                       .append('svg')
-                      .attr('width', width)
-                      .attr('height', height);
+                      .attr('width', width + 60)
+                      .attr('height', height + 60);
 
         //Add legend to y-axis only
         svg.append('text')
-          .attr('x', 200)
-          .attr('y', 200)
-          .text('Time in Minutes')
+           .attr('x', -120)
+           .attr('y', 20)
+           .attr('transform','rotate(-90)')
+           .text('Time in Minutes')
         
+        //Scaling settings
+        const xScale = d3.scaleTime()
+                         .domain([yearMin,yearMax])
+                         .range([0,width]);
+
+        const xAxis = d3.axisBottom().scale(xScale);
+        //Add x-axis
+        svg.append('g')
+           .call(xAxis)
+           .attr('id','x-axis')
+           .attr('transform','translate(25,520)')
+
 
     })
     .catch(e => console.log(e))
